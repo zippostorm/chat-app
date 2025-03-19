03e1805eda9da828bb516cbb2297a934264164fd
zippostorm <toghkklli@gmail.com>
2025-03-19 14:00:13 +0200

    setup cloudinary + create protect middleware and create update profile api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
@@ -82,6 +84,31 @@ export const logout = (req, res) => {
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
     console.log("Error in logout controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in updateProfile controller: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,19 @@
 import express from "express";
 
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
