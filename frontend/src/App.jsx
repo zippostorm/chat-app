8cda474787fe1284dab35dff4804ab78cdf22f5e
zippostorm <toghkklli@gmail.com>
2025-03-20 14:00:42 +0200

    daisyui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-red-500">My application</div>;
+  return (
+    <div className="">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
 };
 
 export default App;
